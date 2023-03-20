ae6f4a499cfe0231257a5efc995d656f04fbc3f3
Thiago <thiago.antonio.ferreira@gmail.com>
2023-03-20 18:16:25 -0300

    AddContext

---
+++ GraphQLExample.Infra/GraphQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLExample.Infra
+{
+    public class GraphQLContext : DbContext
+    {
+        public GraphQLContext(DbContextOptions<GraphQLContext> opcoes) : base(opcoes)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
